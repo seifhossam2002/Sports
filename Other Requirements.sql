a2b3dd2a957b611a7a067aa026f5854071d50acb
Seif Hossam <seif.hossam26092002@gmail.com>
2022-12-10 02:29:16 +0200

    Add project files.

---
+++ Other Requirements.sql
@@ -0,0 +1,17 @@
+﻿GO;
+CREATE PROCEDURE addAssociationManager
+@name varchar(20),
+@username varchar(20),
+@password varchar(20)
+AS
+INSERT INTO dbo.systemUser VALUES(@username,@password)
+INSERT INTO dbo.allAssocManagers VALUES(@name,@username);
+
+-------------------
+GO;
+CREATE PROCEDURE addNewMatch
+@clubname1 varchar(20),
+@clubname2 varchar(20),
+@hostname varchar(20),
+@starttime datetime
+AS
