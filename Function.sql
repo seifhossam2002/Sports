2830a4c4d083ae97376018da6de875daeee5d001
Seif Hossam <seif.hossam26092002@gmail.com>
2022-12-13 04:56:03 +0200

    UPDATE

---
+++ Function.sql
@@ -0,0 +1,33 @@
+﻿GO;
+CREATE FUNCTION viewAvailableStadiumsOn
+(@elyoom datetime)
+returns @availablestadium TABLE(
+	name varchar(20),
+	location varchar(20),
+	capacity int
+)
+AS
+BEGIN
+	INSERT INTO @availablestadium
+	SELECT name ,location,capacity from (SELECT name,location,capacity,match.id from stadium
+										LEFT OUTER JOIN match ON match.stadiumId=stadium.id
+										WHERE stadium.status=1 and match<>@elyoom) --hya leeh 3'lt?
+	return @availablestadium
+END
+
+-------------
+GO;
+CREATE FUNCTION allUnassignedMatches
+(@nameClub varchar(20))
+returns @unassignedMatchesTable TABLE(
+	guestName varchar(20),
+	startTime varchar(20)
+)
+AS
+BEGIN
+	DECLARE @hostId int,@matchid int;
+	SELECT @hostId=host.id from club host where host.name=@nameClub;
+	INSERT INTO @unassignedMatchesTable
+	SELECT match.club2Id,match.startTime from match where match.stadiumId IS NULL;
+	return
+END
--- Other Requirements.sql
+++ Other Requirements.sql
@@ -159,24 +159,6 @@ SELECT * FROM clubRepresentative
 EXEC addRepresentative 'seif','club1','seifhossam262002','Seif-2609'
 
 -------------
-GO;
-CREATE FUNCTION viewAvailableStadiumsOn
-(@elyoom datetime)
-returns @availablestadium TABLE(
-	name varchar(20),
-	location varchar(20),
-	capacity int
-)
-AS
-BEGIN
-	INSERT INTO @availablestadium
-	SELECT name ,location,capacity from (SELECT name,location,capacity,match.id from stadium
-										LEFT OUTER JOIN match ON match.stadiumId=stadium.id
-										WHERE stadium.status=1 and match<>@elyoom) --hya leeh 3'lt?
-	return @availablestadium
-END
-
--------------
 
 GO;
 CREATE PROCEDURE addHostRequest
@@ -198,20 +180,5 @@ INNER JOIN stadium ON stadium.id=match.stadiumId
 where stadium.name=@stadiumname and match.startTime=@datetime
 
 -------------
-GO;
-CREATE FUNCTION allUnassignedMatches
-(@nameClub varchar(20))
-returns @unassignedMatchesTable TABLE(
-	hostName varchar(20),
-	guestName varchar(20),
-	startTime varchar(20)
-)
-AS
-BEGIN
-	DECLARE @hostId int,@matchid int;
-	SELECT @hostId=host.id from club host where host.name=@nameClub;
-	INSERT INTO @unassignedMatchesTable
-	SELECT match.club1Id,match.club2Id,match.startTime from match where match.stadiumId IS NULL;
-	return
-END
+
 
